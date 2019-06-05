71122590a3484f48ae08570b3cae1c0c34d0d0fb
minhmo <sipko.tomas@gmail.com>
2019-06-05 20:21:58 +0200

    locust addition and fixes.

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ class SimpleRedisBehaviour(TaskSet):
     item_ids = []
     ord_ids = []
 
+    # Users
     @task
     def user_create(self):
         resp = self.client.post("/users/create")
@@ -15,8 +16,37 @@ class SimpleRedisBehaviour(TaskSet):
 
         self.user_ids.append(json_resp["id"])
 
-        # pydevd.settrace('localhost', port=12345, stdoutToServer=True, stderrToServer=True)
+    @task
+    def user_credit_add(self):
+        if len(self.user_ids) < 1:
+            return
 
+        resp = self.client.post("/users/credit/add/" + self.get_rand_user_id() + "/" + str(randint(1, 100)))
+
+    @task
+    def user_credit_add(self):
+        if len(self.user_ids) < 1:
+            return
+
+        resp = self.client.get("/users/credit/" + self.get_rand_user_id())
+
+    @task
+    def user_delete(self):
+        if len(self.user_ids) < 1:
+            return
+
+        user_id = self.get_rand_user_id()
+        resp = self.client.delete("/users/remove/" + user_id)
+
+        self.user_ids.remove(user_id)
+
+    @task
+    def user_find(self):
+        if len(self.user_ids) < 1:
+            return
+        resp = self.client.get("/users/find?ids[]=" + self.get_rand_user_id())
+
+    # Stock
     @task
     def stock_create(self):
         resp = self.client.post("/stock/item/create")
@@ -29,51 +59,103 @@ class SimpleRedisBehaviour(TaskSet):
     @task
     def stock_availability(self):
         if len(self.item_ids) < 1:
-            pass
+            return
 
         resp = self.client.get("/stock/availability/" + self.get_rand_item_id())
 
     @task
     def stock_add(self):
         if len(self.item_ids) < 1:
-            pass
+            return
 
-        resp = self.client.post("/stock/add/" + self.get_rand_item_id() + "/" + randint(1, 100))
+        resp = self.client.post("/stock/add/" + self.get_rand_item_id() + "/" + str(randint(1, 100)))
 
     # ORDERS
     @task
     def orders_create(self):
         if len(self.user_ids) < 1:
-            pass
+            return
 
         resp = self.client.post("/orders/create/" + self.get_rand_user_id())
         json = resp.json()
 
-        self.ord_ids.append(json["id"])
+        self.ord_ids.append(json["orderId"])
 
     @task
     def orders_add_item(self):
         if len(self.item_ids) < 1 or len(self.ord_ids) < 1:
-            pass
+            return
 
         resp = self.client.post("/orders/additem/" + self.get_rand_ord_id() + "/" + self.get_rand_item_id())
 
     @task
-    def orders_remove_item(self):
-        if len(self.item_ids) < 1 or len(self.ord_ids) < 1:
-            pass
+    def orders_find(self):
+        if len(self.ord_ids) < 1:
+            return
 
-        resp = self.client.post("/orders/removeitem/" + self.get_rand_ord_id() + "/" + self.get_rand_item_id())
+        resp = self.client.get("/orders/find/" + self.get_rand_ord_id())
+
+    @task
+    def orders_remove_item(self):
+        if len(self.ord_ids) < 1:
+            return
+
+        resp = self.client.get("/orders/find/" + self.get_rand_ord_id())
+        json_resp = resp.json()
+
+        if len(json_resp) < 1 or len(json_resp["orderItems"]) < 1:
+            return
+
+        order_items_keys = list(json_resp["orderItems"].keys())
+
+        arr_len = len(order_items_keys) - 1
+
+        if arr_len == 0:
+            self.client.post("/orders/removeitem/" + order_items_keys[0])
+        else:
+            resp = self.client.post("/orders/removeitem/" + order_items_keys[randint(0, arr_len)])
+
+    @task
+    def orders_checkout(self):
+        if len(self.ord_ids) < 1:
+            return
+
+        resp = self.client.post("/orders/checkout/" + self.get_rand_ord_id())
+
+    # Payment
+
+    @task
+    def payment_status(self):
+        if len(self.ord_ids) < 1:
+            return
+
+        resp = self.client.get("/payment/status/" + self.get_rand_ord_id())
 
     # Helpers
     def get_rand_item_id(self):
-        return self.item_ids[randint(0, len(self.item_ids) - 1)]
+
+        arr_len = len(self.item_ids) - 1
+
+        if arr_len == 0:
+            return self.item_ids[0]
+
+        return self.item_ids[randint(0, arr_len)]
 
     def get_rand_user_id(self):
-        return self.user_ids[randint(0, len(self.user_ids) - 1)]
+        arr_len = len(self.user_ids) - 1
+
+        if arr_len == 0:
+            return self.user_ids[0]
+
+        return self.user_ids[randint(0, arr_len)]
 
     def get_rand_ord_id(self):
-        return self.ord_ids[randint(0, len(self.ord_ids) - 1)]
+        arr_len = len(self.ord_ids) - 1
+
+        if arr_len == 0:
+            return self.ord_ids[0]
+
+        return self.ord_ids[randint(0, arr_len)]
 
 
 class WebsiteUser(HttpLocust):
